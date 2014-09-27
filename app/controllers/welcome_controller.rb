602deb971e9c7d86f69ad32fa2ad4994f19b8ac9
Binit Kumar <binit.kumar.hbs@gmail.com>
2014-09-27 23:21:36 +0530

    MOnkey patch

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,33 +1,41 @@
 class WelcomeController < ApplicationController
   def index
-    if verify_name 
+    username = params[:name]
+
+    if verify_name(username) == true
       render json: {isValid: true}
+    elsif varify_name == "Invalid"
+      render json: {notice: "Invalid paramter"}
     else
       render json: {isValid: false}
     end
   end
 
-  def verify_name
+  def verify_name(name)
     begin
-      @agent = Mechanize.new
-      url = "http://kik.com/u/#{params[:name]}"
+      if name != ''
+        @agent = Mechanize.new
+        url = "http://kik.com/u/#{name}"
        
-      @page = @agent.get("http://kik.com/u/#{params[:name]}")
+        @page = @agent.get(url)
 
-      content = @page.content
-      puts "----------------------------------" 
-      puts url
-      puts "----------------------------------" 
-      puts content.inspect
-      puts "----------------------------------" 
-      puts "----------------------------------" 
-      if Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("USERNAME:#{params[:name]}")
-        return true
-      elsif Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("http\:\/\/kik.com\/profile\/notfound.php")
-        return false
+        content = @page.content
+        puts "----------------------------------" 
+        puts url
+        puts "----------------------------------" 
+        puts content.inspect
+        puts "----------------------------------" 
+        puts "----------------------------------" 
+        if Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("USERNAME:#{params[:name]}")
+          return true
+        elsif Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("http\:\/\/kik.com\/profile\/notfound.php")
+          return false
+        else
+          sleep 1
+          return verify_name
+        end
       else
-        sleep 1
-        return verify_name
+        return "Invalid"
       end
     rescue => exp
       sleep 1
