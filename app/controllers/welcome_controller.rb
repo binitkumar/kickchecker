954b0b4b8c35a3b5ae94d10ffc511e985564df30
Binit Kumar <binit.kumar.hbs@gmail.com>
2014-09-27 22:54:54 +0530

    Updated logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,25 @@
 class WelcomeController < ApplicationController
   def index
+    if verify_name 
+      render json: {isValid: true}
+    else
+      render json: {isValid: false}
+    end
+  end
+
+  def verify_name
     @agent = Mechanize.new
     @page = @agent.get("http://kik.com/u/#{params[:name]}")
 
     content = @page.content
-
-    puts content
+    
     if Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("USERNAME:#{params[:name]}")
-      render json: {isValid: true}
+      return true
+    elsif Nokogiri::HTML(content).text.gsub("\t","").gsub("\n","").match("http\:\/\/kik.com\/profile\/notfound.php")
+      return false
     else
-      render json: {isValid: false}
+      sleep 1
+      return verify_name
     end
   end
 end
