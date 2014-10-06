ad858688c483e3473d8da8cd1d7080a80732ebd7
Binit Kumar <binit.kumar.hbs@gmail.com>
2014-10-06 16:13:39 +0530

    Updated with threads

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,26 +5,38 @@ class WelcomeController < ApplicationController
     usernames = params[:name]
 
     verification_status = Array.new
+    threads = Array.new
+    i = 0
     usernames.split(",").each do |username|
       prev_status = VerifiedName.find_by_username(username)
-
+      i += 1
       if prev_status.nil?
-        status = loop_check(10, username)
-
-        VerifiedName.create(username: username, status: status)
-
-        if status == true
-          verification_status.push(name:username,isValid: true)
-        else
-          verification_status.push(name:username,isValid: false)
+        thread = Thread.new(i) do |i|
+          status_hash = get_hash(username)
+          verification_status.push status_hash
         end
+        threads.push thread
+
       else
         verification_status.push(name:username,isValid: prev_status.status)
       end
     end
+    threads.each {|t| t.join}        
     render json: verification_status
   end
   
+  def get_hash(username)
+    status = loop_check(10, username)
+
+    VerifiedName.create(username: username, status: status)
+
+    if status == true
+      return {name:username,isValid: true}
+    else
+      return {name:username,isValid: false}
+    end
+  end
+
   def loop_check(loop_count, username)
     loop_count.times do 
       return true if verify_name(username) == true
@@ -52,7 +64,7 @@ class WelcomeController < ApplicationController
       end
     rescue => exp
       sleep 1
-      return verify_name
+      return verify_name name
     end
   end
 end
